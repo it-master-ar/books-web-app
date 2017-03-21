dcb6d6a6914cc4dc935d241df3d41c806bc51917
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-03-21 01:49:48 -0300

    Adding env

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,33 @@
+const webpack = require('webpack')
+
+let filename = 'lib/index.js'
+const plugins = []
+
+if (process.env.ENV === 'prod') {
+  filename = 'lib/index.min.js'
+
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      output: {
+        comments: false
+      }
+    }))
+}
+
 module.exports = {
   entry: './src/index.js',
   output: {
     path: __dirname,
-    filename: 'lib/index.js'
+    filename
   },
   module: {
     loaders: [
       { test: /\.css$/, loader: 'style-loader!css-loader' },
       { test: /\.js$/, loader: 'babel-loader' }
     ]
-  }
+  },
+  plugins
 }
