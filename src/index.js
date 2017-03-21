2342bdfb2278222558ffa8691ac90ca5c0beb205
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-03-21 01:13:29 -0300

    Adding babel loader

--- src/index.js
+++ src/index.js
@@ -1,23 +1,27 @@
-// const hello = () => {
-//   console.log('Hello World!')
-// }
-//
-// hello()
-//
-// class Person {
-//   constructor () {
-//     console.log('')
-//   }
-// }
-//
-// const p1 = new Person()
-// p1.hello()
-// p1.hello()
-// p1.hello()
-// p1.hello()
-// p1.hello()
-// p1.hello()
-// p1.hello()
+const hello = () => {
+  console.log('Hello World!')
+}
+
+hello()
+
+class Person {
+  constructor (name = 'John Doe') {
+    this.name = name
+  }
+
+  hello () {
+    console.log(`Hello, my name is ${this.name}`)
+  }
+}
+
+const p1 = new Person()
+p1.hello()
+p1.hello()
+p1.hello()
+p1.hello()
+p1.hello()
+p1.hello()
+p1.hello()
 
 import './css/main.css'
 import text from './second'
