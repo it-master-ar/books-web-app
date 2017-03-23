32bd52aa512302796b666bcc86528f8f0506fd26
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-03-23 10:28:18 -0300

    Refactoring webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,11 @@
-const webpack = require('webpack')
+const isProd = process.env.ENV === 'prod'
 
-let filename = 'lib/index.js'
-const plugins = []
-
-if (process.env.ENV === 'prod') {
-  filename = 'lib/index.min.js'
-
-  plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      },
-      output: {
-        comments: false
-      }
-    }))
-}
+let filename = isProd ? 'lib/index.min.js' : 'lib/index.js'
 
 module.exports = {
   entry: './src/index.js',
 
-  devtool: 'source-map',
+  devtool: isProd ? 'source-map' : false,
 
   output: {
     path: __dirname,
@@ -32,7 +17,5 @@ module.exports = {
       { test: /\.css$/, loader: 'style-loader!css-loader' },
       { test: /\.js$/, loader: 'babel-loader' }
     ]
-  },
-
-  plugins
+  }
 }
