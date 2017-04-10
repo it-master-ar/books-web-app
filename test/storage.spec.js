2b5b9ab8b38f7e4cb3b0bbedc64016116ff81404
Ignacio Anaya <ignacio.anaya89@gmail.com>
2017-04-10 10:26:25 -0300

    Refactoring test

--- test/storage.spec.js
+++ test/storage.spec.js
@@ -7,17 +7,21 @@ describe('Storage', () => {
     window.localStorage.clear()
   })
 
-  it('get', () => {
-    window.localStorage.setItem('test', 1)
-    const result = storage.get('test')
+  describe('get()', () => {
+    it('should get the item', () => {
+      window.localStorage.setItem('test', 1)
+      const result = storage.get('test')
 
-    assert.equal(result, 1)
+      assert.equal(result, 1)
+    })
   })
 
-  it('set', () => {
-    storage.set('test', 1)
-    const result = window.localStorage.getItem('test')
+  describe('set()', () => {
+    it('should set the item', () => {
+      storage.set('test', 1)
+      const result = window.localStorage.getItem('test')
 
-    assert.equal(result, 1)
+      assert.equal(result, 1)
+    })
   })
 })
